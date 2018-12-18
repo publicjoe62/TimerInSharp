1a9c60f7273662e8735f3caa2f288f8f29789d92
Joe Public <publicjoe62@gmail.com>
2018-12-18 14:37:42 +0100

    Initial Project Files

---
+++ TimerInSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimerInSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Timer in C#");
+        }
+    }
+}
