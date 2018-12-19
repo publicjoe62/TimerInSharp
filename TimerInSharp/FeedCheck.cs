feb4f645e66b0218cd8de1a25d72a9283de34b67
Joe Public <publicjoe62@gmail.com>
2018-12-19 13:14:27 +0100

    Add Feedback Class

---
+++ TimerInSharp/FeedCheck.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Threading;
+
+namespace TimerInSharp
+{
+    public class FeedCheck
+    {
+        static int _count = 0;
+        static object _consumingSync = new object();
+        static System.Threading.Timer _produceTimer = null;
+
+        private static void Consume()
+        {
+            if (!Monitor.TryEnter(_consumingSync)) return;
+
+            try
+            {
+                while (_count > 0)
+                {
+                    Interlocked.Decrement(ref _count);
+                }
+            }
+            finally
+            {
+                Monitor.Exit(_consumingSync);
+            }
+        }
+
+        private static void Produce(object state)
+        {
+            Console.WriteLine("Produce");
+            Consume();
+        }
+
+        public static void Start()
+        {
+            // small risk of race condition here, but no necessarily
+            // be bad if multiple Timers existed for a moment, since only
+            // the last one will survice
+            if ( _produceTimer == null )
+            {
+                /*
+                _produceTimer = new System.Threading.Timer(
+                    _ => FeedCheck.Produce(), null, 0, 1000
+                );
+                */
+
+                _produceTimer = new System.Threading.Timer(Produce, null, 0, 1000);
+
+            }
+        }
+    }
+}
--- TimerInSharp/Program.cs
+++ TimerInSharp/Program.cs
@@ -9,7 +9,19 @@ namespace TimerInSharp
         {
             TimerExample te = new TimerExample();
             Console.WriteLine("Timer in C# :: start");
+
+            // FeedCheck
+            //
+            // FeedCheck fc = new FeedCheck();
+            FeedCheck.Start();
+
+            /*
+            // Timer example
+            //
             te.Execute();
+            */
+            Console.WriteLine("press enter to stop");
+            Console.ReadLine();
             Console.WriteLine("Timer in C# :: end");
         }
     }
