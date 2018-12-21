e8082ed2ba34501b827900ca8475e61877606734
Joe Public <publicjoe62@gmail.com>
2018-12-21 12:41:21 +0100

    Add Delegates

---
+++ TimerInSharp/Delegates1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace TimerInSharp
+{
+    delegate int Transformer(int x);
+
+    public class Delegates1 : TISBase
+    {
+
+        public Delegates1()
+        {
+            cout("construct Delegates1");
+        }
+
+        public void execute()
+        {
+            cout("execute Delegates1 ");
+            sample1();
+        }
+
+        private int square(int x)
+        {
+            return x * x;
+        }
+
+        /// <summary>
+        /// Simple delegate
+        /// </summary>
+        public void sample1()
+        {
+            cout("execute sample1 ...");
+            Transformer t = square;
+            cout("t(3)=" + t(3));
+        }
+
+
+    }
+}
